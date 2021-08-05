3578b848dfaa6b971ef48653637e81be9cf87e9d
EzequielCaste <ezequiel.caste@gmail.com>
2021-08-05 17:22:32 -0300

    manejo de errores y validación

--- index.js
+++ index.js
@@ -17,33 +17,42 @@ const testUser = {
 app.post('/login', async (req, res) => {
   const {name, password} = req.body;
 
-  if (
-    name === testUser.name &&
-    bcrypt.compareSync(password, testUser.password)
-  ) {
-    const token = jwt.sign(
-      {
-        name,
-        password: testUser.password,
-      },
-      process.env.JWT_SECRET
-    );
+  try {
+    if (
+      name === testUser.name &&
+      bcrypt.compareSync(password, testUser.password)
+    ) {
+      const token = jwt.sign(
+        {
+          name,
+          password: testUser.password,
+        },
+        process.env.JWT_SECRET
+      );
 
-    res.status(200).json({
-      ok: true,
-      msg: 'Correcto!',
-      token,
-    });
+      res.status(200).json({
+        ok: true,
+        msg: 'Correcto!',
+        token,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).send('Datos incorrectos');
   }
-  res.status(400).send('Datos incorrectos');
 });
 
 app.get('/saludo', (req, res) => {
   if (req.body) {
-    const {name} = jwt.decode(req.body.token);
-    res.status(200).send(`Buenos días ${name}!`);
+    try {
+      const {name} = jwt.verify(req.body.token, process.env.JWT_SECRET);
+      res.status(200).send(`Buenos días ${name}!`);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  } else {
+    res.status(400).send('Error de solicitud');
   }
-  res.status(400).send('Error de solicitud');
 });
 
 app.listen(3000, console.log('Listo!'));
